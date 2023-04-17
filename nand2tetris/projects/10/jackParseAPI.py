504a5a72f43ab6688788ed4f94392dcbcd0f6a48
DaltonLee2k <77364619+DaltonLee2k@users.noreply.github.com>
2023-04-16 22:37:15 -0600

    Jack code compiler

--- nand2tetris/projects/10/jackParseAPI.py
+++ nand2tetris/projects/10/jackParseAPI.py
@@ -1,4 +1,6 @@
 
+import re
+
 keywords = ['boolean', 'char', 'class', 'constructor', 'do', 'else', 'false', 'field', 'function', 'if', 'int', 'let', 'method', 'null', 'return', 'static', 'this', 'true', 'var', 'void', 'while']
 symbols = '{}()[].,;+-*/&|<>=~'
 numberChars = '0123456789'
@@ -11,14 +13,48 @@ def pass_1(file):
     file = clean_file(file)
     with open(file,'r') as f:
         lines = f.readlines()
-    for line in lines:
-        print(line)
     pass_2(lines)
 
+def getToken(symbol):
+    if symbol == "<":
+        return("<symbol>"+"&lt;"+"</symbol>")
+    if symbol == ">":
+        return("<symbol>"+"&gt;"+"</symbol>")
+    
+    
+    #determine XML output based on Token
+    if symbol in keywords:
+        return("<keyword>"+symbol+"</keyword>")
+    elif symbol in symbols:
+        return("<symbol>"+symbol+"</symbol>")
+    elif symbol[0] in numberChars:
+        return("<integerConstant>"+symbol+"</integerConstant>")
+    elif symbol[0] in identStart:
+        return("<identifier>"+symbol+"</identifier>")
+    elif symbol[0] in numberChars:
+        return("<intConst>"+symbol+"</intConst>")
+    elif symbol[0] == '"':
+        symbol = symbol.strip('"')
+        return("<stringConstant>"+symbol+"</stringConstant>")
+    else:
+        return(symbol)
 
 def pass_2(file):
+    out = open('out.xml','w')
+    out.write("<tokens>\n")
     
-    pass
+
+    for line in file:
+        #splits line from cleaned file based on characters
+        words = re.findall(r'[{}()[\].,;+\-*/&|<>=~]|".+?"|\w+', line) #from https://stackoverflow.com/questions/40734235/how-to-use-re-findall-in-python
+        #\w strings of words to be returned as a single word
+        for word in words:
+            out.write(getToken(word)+"\n")
+
+    out.write("</tokens>")
+
+        
+
 
 
 def clean_file(file):
